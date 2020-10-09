e2589cc4b45b7ef79ded0dc7c911465ad6a67f7b
github cacaox <worktype25@gmail.com>
2020-10-09 22:53:36 +0900

    7day 6dayのコードのリファクタリング

--- 6day/main.js
+++ 6day/main.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const el = document.querySelector('.animate-title');
-  const str = el.innerHTML.trim().split("");
- // let concatStr = '';
-
- // for(let c of str) {
- //   c = c.replace(' ', '&nbsp;');
- //   concatStr += `<span class="char">${c}</span>`
- // }
- // el.innerHTML = concatStr;
-
- el.innerHTML = str.reduce((aucc, cuur) => {
-     cuur = cuur.replace(' ', '&nbsp');
-     return `${aucc}<span class="char">${cuur}</span>`;
- }, "");
+ const btn = document.querySelector('.btn');
+ const ani = new TextAnimate('.animate-title');
+ ani.animate();
+ btn.addEventListener('click', ani.animate.bind(ani));
 });
+
+class TextAnimate {
+    constructor(el){
+         this.el = document.querySelector(el);
+         this.str = this.el.innerHTML.trim().split("");
+         this.el.innerHTML = this._sptext();
+    }
+    _sptext() {
+        return this.str.reduce((acc, curr) => {
+            curr = curr.replace(/\s+/, '&nbsp;');
+            return `${acc}<span class="char">${curr}</span>`;
+        }, "");
+    }
+    animate() {
+        this.el.classList.toggle('inview');
+    }
+}
